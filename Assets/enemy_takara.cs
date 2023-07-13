388166fdb59fcbc70153e85462f11d761e5fd5f4
祐希 <2312110011v@kindai.ac.jp>
2023-07-13 15:43:33 +0900

    script作ったけどerror出てるの治されへん...この前のyoutubeのゲーム開発編~RPG編~の10分あたりです。 Textっていうやつがないねんなー 14,15行目のTextってやつにerror出てます。

---
+++ Assets/enemy_takara.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_takara : MonoBehaviour
+{
+    public string name = "";
+    public string[] talk = {};
+    public int talk_count = 0;
+    SpriteRenderer sr;
+    public Sprite[] skin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sr.sprite = skin[talk_count];
+    }
+}
